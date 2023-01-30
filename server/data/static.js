030f41742681916d286c933be1339e8be4edf78f
ADMIN <thikimhoa.py@gmail.com>
2023-01-30 16:08:15 +0700

    first commit

---
+++ server/data/static.js
@@ -0,0 +1,35 @@
+const books = [
+    {
+        id: 1,
+        name: 'sachs 1',
+        genre: 'adventure'
+    },
+    {
+        id: 2,
+        name: 'sachs 2',
+        genre: 'education'
+    }
+]
+
+const authors = [
+    {
+        id: 1,
+        name: 'ngo tat to',
+        age: 60
+    },
+    {
+        id: 2,
+        name: 'ngo tat to 2',
+        age: 70
+    },
+    {
+        id: 3,
+        name: 'ngo tat to3',
+        age: 80
+    },
+]
+
+module.exports = {
+    books,
+    authors
+}
---
+++ server/resolver/resolver.js
@@ -0,0 +1,12 @@
+const {books, authors} = require('../data/static')
+
+const resolvers = {
+    Query: {
+        books: () => books,
+        book: (parent, args) => books.find(book => book.id.toString() === args.id),
+        authors: () => authors,
+        author: (parent, args) => authors.find(author => author.id == args.id)
+    }
+}
+
+module.exports = resolvers
